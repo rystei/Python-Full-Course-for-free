13a4bdae421ad3ca20ccf292776ae867c1626df6
Your Name <gustavo.telles@edu.unifil.br>
2025-04-19 12:42:14 -0300

    Variáveis

---
+++ variaveis.py
@@ -0,0 +1,25 @@
+# Variáveis = se refere a um valor especifico, entre elas existem (string, integer, floar, boolean).
+# É tipo uma caixinha onde você pode guardar um número, um texto, uma lista ou qualquer outro tipo de dado. Você dá um nome a essa caixinha para que possa encontrar o valor armazenado mais tarde.
+
+# strings
+first_name = "rystei"
+food = "Xbacon"
+email = "rystei123@fake.com"
+
+#Intergers
+age = 25
+quantity = 3
+numero_de_cachorros = 12
+
+
+#Float
+price = 10.22
+distancia = 123.43
+nota = 74.7
+
+#boolean
+
+is_student = True
+for_sale = True
+is_online = False
+
