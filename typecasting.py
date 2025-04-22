fc0e539d070c40e00da0b66b6106592031740194
rystei <gustavo.telles@edu.unifil.br>
2025-04-22 09:33:30 -0300

    aula de typecasting upload.

---
+++ typecasting.py
@@ -0,0 +1,19 @@
+#Typecasting = é o processo de converter uma variavel de um determinado tipo para outro como
+# str(), int(), float(), bool()
+
+nome = "bro code"
+idade = 20
+gpa = 3.2
+is_student = True
+
+gpa = int(gpa)
+print(type(gpa))
+print(gpa)
+
+idade = float(idade)
+print(type(idade))
+print(idade)
+
+idade = str(idade)
+print(type(idade))
+print(idade)
