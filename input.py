b2bc83f46e5f99a2c5d64016392c94fe4853d9be
rystei <gustavo.telles@edu.unifil.br>
2025-04-23 13:53:51 -0300

    aula de input upload.

---
+++ input.py
@@ -0,0 +1,30 @@
+# input() = uma função que tem como função capturar as informações inseridas nele pelo usuário
+
+#name = input("Qual o seu nome?: ")
+#idade = int(input("Qual sua idade?: "))
+#idade = idade + 1
+#print(f"Oi {name}")
+#print("Feliz aniversario")
+#print(f"Você tem agora {idade} anos")
+
+
+#exercicio 1 calcular a área de um retangulo
+
+#comprimento = float(input("Qual o comprimento?:"))
+#largura = float(input("Qual a largura?:"))
+#area = comprimento * largura
+
+#print(f"O tamanho da área é: {area}cm²")
+
+
+#exercicio 2 items de compra
+
+item = input("O que você gostaria de comprar?: ")
+preco = float(input("Quanto custa?: "))
+quantidade = int(input("Quantos você gostaria de comprar?: "))
+
+total = preco * quantidade
+
+print(f"Você comprou {quantidade} {item}")
+print(f"Sua compra deu um total de R$: {total}")
+print("Qual seria a forma de pagamento?")
