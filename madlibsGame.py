2a3d3b5fa1eb20de537876462a59130b6aa27516
rystei <gustavo.telles@edu.unifil.br>
2025-04-24 13:27:31 -0300

    aula de criação madlibs game, um jogo onde você cria uma historia colocando palavra nos espaços vazios (upload).

---
+++ madlibsGame.py
@@ -0,0 +1,14 @@
+#madlibs Game
+# Um jogo de palavras onde você cria uma historia
+# Preecnhendo os espaços em branco com palavras aleatorias
+
+adjetivo1 = input("Insira um adjetivo (descrição): ")
+algo1 = input("Insira algo (pessoa, lugar, coisa): ")
+adjetivo2 = input("Insira um adjetivo (descrição): ")
+verbo1 = input("Insira um verbo (uma ação): ")
+adjetivo3 = input("Insira um adjetivo (descrição): ")
+
+print(f"Hoje eu fui em um zoologico {adjetivo1}.")
+print(f"Em uma exibição eu vi {algo1}.")
+print(f"{algo1} estava {adjetivo2} e {verbo1}")
+print(f"Eu estava {adjetivo3}!")
