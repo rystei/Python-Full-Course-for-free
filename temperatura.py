399dc9d2e9e197c620c9f98caddf921ad8c9a416
rystei <gustavo.telles@edu.unifil.br>
2025-05-05 10:23:29 -0300

    temperatura em Celsius ou Fahrenheit arrumando ordem.

--- temperatura.py
+++ temperatura.py
@@ -2,10 +2,10 @@
 unidade = input("Insira se você quer uma temperatura em Celsius ou Fahrenheit (C/F): ").upper()
 temp = float(input("Insira a temperatura: "))
 
-if unidade == "C":
+if unidade == "F":
     temp = round((9 * temp) / 5 + 32, 1)
     print(f"A Temperatura em Fahrenheit é: {temp}°F ")
-elif unidade == "F":
+elif unidade == "C":
     temp = round((temp - 32) * 5 / 9, 1)
     print(f"A Temperatura em Celsius é: {temp}°C ")
 else:
